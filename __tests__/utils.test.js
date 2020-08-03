259122c477f43615891b1ef222e625acdd0f384f
AndyBrowbank <abrowbank@hotmail.com>
2020-08-03 21:17:57 +0100

    migrations done

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -4,7 +4,36 @@ const {
   formatComments,
 } = require('../db/utils/utils');
 
-describe('formatDates', () => {});
+describe('formatDates', () => {
+  test('returns an empty array when passed an empty array', () => {
+    expect(formatDates([])).toEqual([])
+  });
+  test('takes an array of objects and returns a new array', () => {
+    const input = [ { testkey : "testvalue"}];
+    const actual = formatDates(input);
+    expect(actual).not.toBe(input)
+  });
+  test('does not mutate original array', () => {
+    const input = [ { testkey : "testvalue"}];
+    const control = formatDates(input);
+    expect(input).toBe(control)
+  });
+  test('converts timestamp at single created_at key to JS date object', () => {
+    const input = [{body: 'Lobster pot',
+    belongs_to: 'Living in the shadow of a great man',
+    created_by: 'icellusedkars',
+    votes: 0,
+    created_at: 1322138163389
+    }];
+    const expected = [{body: 'Lobster pot',
+    belongs_to: 'Living in the shadow of a great man',
+    created_by: 'icellusedkars',
+    votes: 0,
+    created_at:new Date(1322138163389)
+    }]
+    expect(formatDates(input)).toEqual(expected)
+  });
+});
 
 describe('makeRefObj', () => {});
 
--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,20 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+    if (list.length===0){ return []}
+ 
+//     list.forEach((item) => {
+// console.log(new Date(item.created_at))
+//     })
+
+const timeStamp = list[0].created_at;
+const newTimeStamp = new Date(timeStamp);
+const object = list[0]
+object.created_at = newTimeStamp
+return [object];
+ 
+ 
+ 
+
+};
 
 exports.makeRefObj = list => {};
 
